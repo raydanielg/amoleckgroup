e0a048054f8638471c911999d740f9d9cc3803e0
raydanielg <airezra2+1@gmail.com>
2026-07-27 11:33:01 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,26 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+Route::get('/divisions/ames', function () {
+    return view('divisions.ames');
+})->name('divisions.ames');
+
+Route::get('/divisions/aphamko', function () {
+    return view('divisions.aphamko');
+})->name('divisions.aphamko');
+
+Route::get('/divisions/asca', function () {
+    return view('divisions.asca');
+})->name('divisions.asca');
+
+Route::get('/divisions/physiotherapy', function () {
+    return view('divisions.physiotherapy');
+})->name('divisions.physiotherapy');
+
+Route::get('/divisions/amotech', function () {
+    return view('divisions.amotech');
+})->name('divisions.amotech');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
